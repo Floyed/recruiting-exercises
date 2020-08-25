5d4dbc5b2b1bebbc1eec9f58edc0b23d58456338
Floyed <floyedpinto08@gmail.com>
2020-08-25 15:59:34 -0400

    Adding additional test cases and folder structure formatiing

---
+++ inventory-allocator/src/tests/data/data9.js
@@ -0,0 +1,6 @@
+exports.data = {
+  items: {},
+  warehouses: [{ name: "owd", inventory: { apple: 5 } }, { name: "dm", inventory: { apple: 5 }}]
+};
+
+exports.result = [];
--- inventory-allocator/src/tests/tests.js
+++ inventory-allocator/src/tests/tests.js
@@ -1,29 +1,33 @@
 var assert = require('assert');
-let index = require('../src/index');
+let index = require('../index');
 let data1 = require('./data/data1');
 let data2 = require('./data/data2');
 let data3 = require('./data/data3');
 let data4 = require('./data/data4');
 let data5 = require('./data/data5');
 let data6 = require('./data/data6');
+let data7 = require('./data/data7');
+let data8 = require('./data/data8');
+let data9 = require('./data/data9');
+let data10 = require('./data/data10');
 
 
-describe('Data 1', function () {
+describe('Items split among two warehouses', function () {
     it('should be equal to the result in data1.js', function () {
         let response = index.calculate(data1.data);
         assert.deepEqual(response, data1.result);
     });
 });
 
-describe('Data 2', function () {
+describe('Equal split between two warehouses', function () {
     it('should be equal to the result in data2.js', function () {
         let response = index.calculate(data2.data);
         assert.deepEqual(response, data2.result);
     });
 });
 
-describe('Data 3', function () {
-    it('Empty array in result check', function () {
+describe('Empty result set', function () {
+    it('must not allocate any resources', function () {
         let response = index.calculate(data3.data);
         assert.deepEqual(response, data3.result);
     });
@@ -47,5 +51,33 @@ describe('Should split an item across warehouses if that is the only way to comp
     it('Must split the allocations', function () {
         let response = index.calculate(data6.data);
         assert.deepEqual(response, data6.result);
+    });
+});
+
+describe('No resources, no warehouses', function () {
+    it('Must give empty allocations', function () {
+        let response = index.calculate(data7.data);
+        assert.deepEqual(response, data7.result);
+    });
+});
+
+describe('No warehouses', function () {
+    it('Must give empty allocations', function () {
+        let response = index.calculate(data8.data);
+        assert.deepEqual(response, data8.result);
+    });
+});
+
+describe('No resources', function () {
+    it('Must give empty allocations', function () {
+        let response = index.calculate(data9.data);
+        assert.deepEqual(response, data9.result);
+    });
+});
+
+describe('First warehouse meets all requirements', function () {
+    it('Must allocate only to first', function () {
+        let response = index.calculate(data10.data);
+        assert.deepEqual(response, data10.result);
     });
 });
